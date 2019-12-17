ea51750ec5e4e9a6dd7b7027be31478e289c3e39
AJ <aayush.jaiswal984@gmail.com>
2019-12-17 10:37:07 +0530

    Today notes & Task

---
+++ testing_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Header from './component/header/Header';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello World</h1>
+    <Header/>
+    </div>
+  );
+}
+
+export default App;
