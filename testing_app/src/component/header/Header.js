e3e3ce02dd968d4fb6f08d163d665497f4eb5b23
AJ <aayush.jaiswal984@gmail.com>
2019-12-23 09:42:10 +0530

    Today Notes

--- testing_app/src/component/header/Header.js
+++ testing_app/src/component/header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Button } from 'react-bootstrap';
+
 class Header extends React.Component {
     render() {
         return (
-            <h1>Hello world from Header</h1>
+            <div>
+                <h1>Hello world from Header</h1>
+                <Button variant="primary">Primary</Button>
+                <Button variant="info">Button</Button>
+            </div>
         )
     }
 }
