34d68f248165cfceb4d56e6b71178b454fa07451
AJ <aayush.jaiswal984@gmail.com>
2020-01-07 16:47:35 +0530

    Test data

--- testing_app/src/App.js
+++ testing_app/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import './dist/css/App.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-import {
-  BrowserRouter as Router, Route
-} from 'react-router-dom';
-
-import Menubar from './component/MenuBar';
-import Header from './component/header/Header';
-import Velit from './component/header/Velit';
+import Header from './component/Header';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Menubar />
-        <Route exact path="/" component={Header} />
-        <Route exact path="/Velit" component={Velit} />
-      </div>
-    </Router>
+    <div>
+      <Header/>
+    </div>
   );
 }
 export default App;
